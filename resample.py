633e5459dec11b78b55867eccd54ae372590a640
Kevin Greif <kgreif@uci.edu>
2022-11-09 23:56:29 -0800

    Final resampling script

--- resample.py
+++ resample.py
@@ -12,64 +12,94 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--in', type=str, required=True,
+parser.add_argument('--infile', type=str, required=True,
                     help='The input file name')
 parser.add_argument('--branch', type=str, required=True,
                     help='The name of the jet pt branch')
 parser.add_argument('--n_jets', type=int, required=True,
                     help='The number of jets to have in the final set')
-parser.add_argument('--out', type=str, required=True,
+parser.add_argument('--outfile', type=str, required=True,
                     help='The output file name')
-parser.parser_args()
+args = parser.parse_args()
 
+print("Resampling jets from {}".format(args.infile))
 
-# Set step size for reading / writing between h5 files
+# Set step size for reading from input file
 stepsize = 1000000
 
-# Pull jet pt from input file
-input = h5py.File(args.in, 'r')
-jet_pt = input[args.branch][:]
-weights = input['weights'][:]
+# Open files
+input_file = h5py.File(args.infile, 'r')
+output_file = h5py.File(args.outfile, 'w')
+n_input_jets = input_file.attrs.get('num_jets')
 
-# Get indeces of jets to keep
+# Find the number of jets we should sample in each step
+n_sample = int(args.n_jets * stepsize / n_input_jets)
+print("Will sample {} jets in each step".format(n_sample))
+
+# Initialize random number generator
 rng = np.random.default_rng()
-keep = rng.choice(numpy.arange(len(weights)), size=args.n_jets, p=weights)
 
-# Open output file
-output = h5py.File(args.out, 'w')
+# Initialize counters
+in_start = 0
+out_start = 0
 
-# Loop through keys in input file
-for key in input.keys():
-
-    # Get input branch details and create output branch
-    in_shape = input[key].shape
-    in_type = input[key].dtype
+# Create branches in output file
+# Unfortunately to do this we need to loop through the branches of the input
+# file before the main processing loop
+print("Creating output file at {}".format(args.outfile))
+for key in input_file.keys():
+    in_shape = input_file[key].shape
+    in_type = input_file[key].dtype
     out_shape = (args.n_jets,) + in_shape[1:]
-    output.create_dataset(key, out_shape, dtype=in_type)
+    output_file.create_dataset(key, out_shape, dtype=in_type)
 
-    # Initialize counters
-    in_start = 0
-    out_start = 0
+# Start reading from input file
+# Continue to read until we have amassed the desired number of jets
+while out_start < args.n_jets:
 
-    # Loop over key in input file
-    while start < args.n_jets:
+    # Pull weights in this batch of input jets
+    in_stop = in_start + stepsize
+    weights = input_file['weights'][in_start:in_stop]
+    weights /= weights.sum()
 
-        # Pull this batch and keep indeces within
-        in_stop = in_start + stepsize
-        batch = input[key][start:stop,...]
-        batch_idx = keep[(start <= keep)*(keep < stop)]
+    # Find indeces for writing to output file
+    out_stop = out_start + n_sample
+    if out_stop > args.n_jets:
+        out_stop = args.n_jets
+        # Since this is the last batch, setting n_sample to be the # of jets
+        # we still need
+        n_sample = out_stop - out_start
+
+    print("\nPulling infile from {} to {}".format(in_start, in_stop))
+    print("Writing to outfile from {} to {}".format(out_start, out_stop))
+    print("Sampling {} jets".format(n_sample))
+
+    # Decide which jets in this batch to keep
+    keep = rng.choice(np.arange(len(weights)), size=n_sample, p=weights, replace=True)
+
+    # Loop through keys in input file
+    for key in input_file.keys():
+
+        # Pull this batch
+        batch = input_file[key][in_start:in_stop,...]
 
         # Resample jets
-        batch = batch[batch_idx,...]
+        batch = batch[keep]
 
         # Write jets
-        out_stop = out_start + len(batch_idx)
-        output[key][out_start:out_start,...] = batch
+        output_file[key][out_start:out_stop,...] = batch
 
-        # Advance counters
-        in_start = in_stop
-        out_start = out_stop
+    # Advance counters
+    in_start = in_stop
+    out_start = out_stop
+
+# Copy attributes dictionary from input file
+for key, attr in input_file.attrs.items():
+    output_file.attrs.create(key, attr)
+
+# Fix num jets attribute
+output_file.attrs['num_jets'] = args.n_jets
 
 # Finish by closing files
-input.close()
-output.close()
+input_file.close()
+output_file.close()
